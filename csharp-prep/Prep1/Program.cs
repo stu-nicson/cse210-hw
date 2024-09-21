df3a511ec0805568c804d24326ed5f1a46b2c76f
Nicson White <whi19048@byui.edu>
2024-09-21 17:40:19 -0600

    Added Name and display

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your Last Name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }

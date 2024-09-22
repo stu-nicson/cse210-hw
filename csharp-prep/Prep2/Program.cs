56e59dc4e24b3becf15944da73f3e50632d3997d
Nicson White <whi19048@byui.edu>
2024-09-21 18:28:52 -0600

    Adding grading System

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter;
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You have a {letter}");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("Congrats on the passing grade! Keep it up.");
+        }
+        else 
+        {
+            Console.WriteLine("Not quite a passing grade! You got it next time!");
+        }
+
     }
 }
